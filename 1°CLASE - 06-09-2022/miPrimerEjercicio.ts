dfe498616901f079e8dd42b711b767407953ec88
Migue <miguelovejero@tdf.edu.ar>
2022-09-13 01:05:16 -0300

    subo ejemplo primer clase librerias

---
+++ 1°CLASE - 06-09-2022/miPrimerEjercicio.ts
@@ -0,0 +1,11 @@
+//console.log("Hola Miguel");
+
+/*let readlineSync = require('readline-sync');
+let nomApe = readlineSync.question("NOMBRE Y APELLIDO: ");
+let edad = readlineSync.question("EDAD: ");
+console.log(nomApe);
+console.log(edad);*/
+
+let readlineSync = require('readline-sync');
+let alturaPersona = readlineSync.question("Indique la altura de la persona: ");
+console.log(alturaPersona);
